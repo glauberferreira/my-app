906fd1fef67e9b6fa77ff6518fd30a3a77b08386
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-08-19 09:26:10 -0300

    Implementar o logoff

--- app/home.js
+++ app/home.js
@@ -1,14 +1,24 @@
 import { View, Text } from 'react-native';
 import { auth } from '../firebaseConfig';
+import { signOut } from 'firebase/auth';
+import { useRouter } from 'expo-router';
+import { Button } from 'react-native-paper';
 
 const Home = () => {
   const user = auth.currentUser;
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    router.replace('/');
+  }
 
   return (
     <View>
       <Text>Home</Text>
       <Text>{user.uid}</Text>
       <Text>{user.email}</Text>
+      <Button onPress={handleLogout}>Sair</Button>
     </View>
   )
 }
